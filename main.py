6adc3b24e5bce415ec7fc11bf4abcc01cc62dd6a
Yvaine Liew <yvaine8888@gmail.com>
2024-09-24 23:02:44 +0000

    sadjsal

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 print('Welcome to the Elite 101 Chatbot!')
 user_name = input('What is your name?: ')
-print('Nice to meet you, ' + user_name)
+print('Nice to meet you, ' + user_name + '!')
 age = input('How old are you?: ')
-print('Oh, to be ' + age + ' again. How can I help you today?')
+
+if age < 15:
+    print('Oh, to be ' + age + ' again.')
+elif age < 18:
+    print("You're " + age + '?') 
+elif age < 55:
+    print("You're " + age + '? Wow...')
+elif age < 120:
+    print("You're " + age + 'Wow, you're old.')
+elif age >= 120:
+    print(age + '?' + 'Now I think you are lying.')
+else:
+    print('You didn't need to lie. It's just your age.')
 print()
+print('What can I do for you today?')
+print()
+
 while True:
     print('--------------------------------------------')
     print('Please choose from the following options:')
@@ -12,6 +27,7 @@ while True:
     print('3. Option3')
     print('4. Exit the conversation.')
     user_choice = input('Please choose a number 1-4: ')
+    print()
     if user_choice == '1':
         print('Option1')
     elif user_choice == '2':
