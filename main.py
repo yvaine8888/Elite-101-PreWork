09caf43323a2d745bc96d8a58826e6e3344cc49c
Yvaine Liew <yvaine8888@gmail.com>
2024-09-24 23:20:04 +0000

    Fixing errors

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ print('Welcome to the Elite 101 Chatbot!')
 user_name = input('What is your name?: ')
 print('Nice to meet you, ' + user_name + '!')
 age = input('How old are you?: ')
+age = int(age)
 
 if age < 15:
-    print('Oh, to be ' + age + ' again.')
+    print(f'Oh, to be {age} again.')
 elif age < 18:
-    print("You're " + age + '?') 
+    print(f"You're {age}?") 
 elif age < 55:
-    print("You're " + age + '? Wow...')
+    print(f"You're {age}? Wow...")
 elif age < 120:
-    print("You're " + age + 'Wow, you're old.')
+    print(f"You're {age}? Wow, you're old.")
 elif age >= 120:
-    print(age + '?' + 'Now I think you are lying.')
+    print(f"You're {age}? Now you are lying.")
 else:
-    print('You didn't need to lie. It's just your age.')
+    print(f"{age} would not work. You didn't need to lie")
 print()
 print('What can I do for you today?')
 print()
